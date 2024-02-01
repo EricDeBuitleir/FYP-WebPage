ffad3b74c382af68aa6fa8b16c7dbdfd6a2b25c1
Eric Butler <72872237+EricDeBuitleir@users.noreply.github.com>
2024-02-01 19:06:24 +0000

    latest

--- fyp-landing-page/src/App.js
+++ fyp-landing-page/src/App.js
@@ -6,23 +6,24 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Eric Butler (20094078) - Computer Science (Automotive & Automation)</h1>
+        <h1>Eric Butler (20094078) </h1>
+        <h7> Computer Science (Automotive & Automation)</h7>
 <p>This is my landing page for my final year project 2023-24.</p>
 <h2> Poster </h2>
 <p> Link to poster: TODO</p>
 <h3> Demo Video</h3>
 <p> Link to demo video: TODO</p>
 <h4> GitHub Repository</h4>
-<p> Link to GitHub repo: TODO</p>
-<h5> TEST</h5>
-        <a
+<p> Link to GitHub repo:
+ <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/EricDeBuitleir/FYP-Public"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+           Here
         </a>
+</p>
       </header>
     </div>
   );
